8b35e79d6b00b14beaa6d6784354ee574a6bbd41
Nathan El <nathan.ellermeier@duke.edu>
2022-06-26 15:09:48 -0400

    re-do offline access (workflow still needs...work)

--- offline_access/runtime.R
+++ offline_access/runtime.R
@@ -1,3 +1,35 @@
 files <- list.files(pattern = "*.Rmd")
 files
-for (f in files) rmarkdown::render(f)
+
+for (f in files){
+  filename <- paste('\'', f, '\'', sep = '')
+  shell(paste("edit_rmd.bat", filename, "\'images/\'", "\'../images/\'"), 
+        intern = FALSE)
+  shell(paste("edit_rmd.bat", filename, "\'files/\'", "\'../files/\'"), 
+        intern = FALSE)
+  shell(paste("edit_rmd.bat", 
+              filename, 
+              "\'assignments/\'", 
+              "\'https://github.com/dghi-biostat/biostatlab/blob/main/assignments/\'"),
+        intern = FALSE)
+  # shell(paste("edit_rmd.bat",
+  #              filename,
+  #              "\'<aside>\'",
+  #              "\'aside: \'"),
+  #        intern = FALSE)
+
+  
+ # shell(paste("edit_rmd.bat",
+ #             filename,
+ #             "\'output: distill::distill_artcile\'",
+ #             "\'output: word_document\nalways_allow_html: true\'"),
+ #       intern = TRUE)
+ # rmarkdown::render(f, "word_document", params = "ask")
+                    #output_options = list(always_allow_html = "true"))
+}
+
+
+for (f in files){
+  rmarkdown::render(f, "word_document", params = "ask")
+                    #output_options = list(always_allow_html = "true"))
+}
